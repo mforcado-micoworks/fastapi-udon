9cd2c91f4ddbd477289304fdca0fa6d252f6ee05
marjo forcado <m.forcado@mico-inc.com>
2026-03-12 16:59:01 +0900

    Add delay

--- oban_workers/send_email.py
+++ oban_workers/send_email.py
@@ -2,6 +2,7 @@
 Oban worker to send emails
 """
 
+import asyncio
 from oban import job
 
 @job(queue="default")
@@ -10,4 +11,8 @@ async def send_email(email: str):
     send an email (simulated)
     """
 
+    await asyncio.sleep(50)
+
     print(f"Sending: {email}")
+
+    return True
