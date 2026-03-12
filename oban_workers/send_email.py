fb5940f66987e19fcf91bcb5b108f77f1510bd2c
marjo forcado <m.forcado@mico-inc.com>
2026-03-12 15:57:05 +0900

    Add doc strings

--- oban_workers/send_email.py
+++ oban_workers/send_email.py
@@ -1,7 +1,15 @@
+"""
+Oban worker to send emails
+"""
+
 from oban import job
 
 @job(queue="default")
 def send_email(email, subject, body):
+    """
+    send an email (simulated)
+    """
+
     print(f"Email: {email}")
     print(f"Subject: {subject}")
     print(f"Body: {body}")
